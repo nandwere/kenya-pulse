bc2ad941feebd5f20da3bc143eb5b1dd192dd1c1
nandwere <mnandwere@gmail.com>
2026-08-01 14:32:14 +0300

    trens route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'));
 router.use('/questions', require('./questions'));
 router.use('/responses', require('./responses'));
 router.use('/counties', require('./counties'));
+router.use('/trends', require('./trends'));
 router.use('/home', require('./home'));
 router.use('/community', require('./community'));
 
